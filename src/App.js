dc7afdd26268926eda000c15a52ecac9799eacc3
Sahil <sahil_hakimi@hotmail.com>
2021-03-15 12:16:58 -0400

    Fixed typo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import profilePic from './img/Sahil-pic.jpg';
 function App() {
   return (
     <div className="App">
-     <h1>My Test Deploy React Appp</h1>
+     <h1>My Test Deploy React App</h1>
      <h2>Author: Sahil Hakimi</h2>
      <img src = {profilePic} alt = "profile-Picture"/>
     </div>
