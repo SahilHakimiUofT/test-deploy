3e4e91b2a0fb3a50267e39295b49b7f7bea52cee
Sahil <sahil_hakimi@hotmail.com>
2021-03-15 12:06:11 -0400

    Deploy my react app to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import profilePic from './img/Sahil-pic.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>My Test Deploy React Appp</h1>
+     <h2>Author: Sahil Hakimi</h2>
+     <img src = {profilePic} alt = "profile-Picture"/>
     </div>
   );
 }
